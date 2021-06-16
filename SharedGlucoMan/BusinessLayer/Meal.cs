a9fff2c50a6e7c4bb0a415f25b06f82add506814
Gabriele MONTI <gamondue@gmail.com>
2021-06-16 18:12:41 +0200

    The prototype of a new Meal class.

---
+++ SharedGlucoMan/BusinessLayer/Meal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlucoMan
+{
+    internal class Meal
+    {
+        List<Food> foodsEaten;
+        DateTime timeBegin;
+        DateTime timeEnd;
+        int accuracyOfChoEstimate; 
+    }
+}
