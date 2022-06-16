066ca5c0d2c5bbdab266aa1b9e65979d94ce1fa6
gamondue@gmail.com <gamondue@gmail.com>
2022-06-16 10:21:24 +0200

    Just begun the UI of frmAlarms

--- GlucoMan.Forms/frmAlarms.Designer.cs
+++ GlucoMan.Forms/frmAlarms.Designer.cs
@@ -31,12 +31,22 @@ namespace GlucoMan.Forms
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAlarms));
             this.btnSetAlarm = new System.Windows.Forms.Button();
-            this.txtHeaderText = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSetAlarm
             // 
-            this.btnSetAlarm.Location = new System.Drawing.Point(28, 128);
+            this.btnSetAlarm.Location = new System.Drawing.Point(292, 111);
             this.btnSetAlarm.Margin = new System.Windows.Forms.Padding(4);
             this.btnSetAlarm.Name = "btnSetAlarm";
             this.btnSetAlarm.Size = new System.Drawing.Size(96, 64);
@@ -44,22 +54,111 @@ namespace GlucoMan.Forms
             this.btnSetAlarm.Text = "Set alarm";
             this.btnSetAlarm.UseVisualStyleBackColor = true;
             // 
-            // txtHeaderText
+            // dateTimePicker1
             // 
-            this.txtHeaderText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtHeaderText.Location = new System.Drawing.Point(124, 13);
-            this.txtHeaderText.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.txtHeaderText.Multiline = true;
-            this.txtHeaderText.Name = "txtHeaderText";
-            this.txtHeaderText.Size = new System.Drawing.Size(149, 117);
-            this.txtHeaderText.TabIndex = 6;
+            this.dateTimePicker1.Location = new System.Drawing.Point(28, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(300, 39);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 252);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(360, 264);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 111);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 64);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(132, 111);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 64);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 39);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 32);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "d";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 32);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "h";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 39);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "2";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 32);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "min";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(259, 60);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(69, 39);
+            this.textBox3.TabIndex = 15;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // frmAlarms
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(280, 208);
-            this.Controls.Add(this.txtHeaderText);
+            this.ClientSize = new System.Drawing.Size(414, 528);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.btnSetAlarm);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -67,6 +166,7 @@ namespace GlucoMan.Forms
             this.Name = "frmAlarms";
             this.Text = "Alarms";
             this.Load += new System.EventHandler(this.frmAlarms_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,6 +175,15 @@ namespace GlucoMan.Forms
         #endregion
 
         private Button btnSetAlarm;
-        private TextBox txtHeaderText;
+        private DateTimePicker dateTimePicker1;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
     }
 }
