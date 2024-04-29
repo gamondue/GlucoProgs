b3034ad73d4c86a97b219b4a888d2060f768b5b0
gamon su Vegetto <gamondue@gmail.com>
2024-04-29 19:17:44 +0200

    Drawing circles on the image of BackPicturePage now works!
    Modified BackPicturePage.xaml and BackPicturePage.xaml.cs to accomplish the drawing of the ONE circle-.
    Added CirclesDrawable, IDrawable class that implement the Method Draw().

--- GlucoMan.Maui/BackPicturePage.xaml.cs
+++ GlucoMan.Maui/BackPicturePage.xaml.cs
@@ -2,10 +2,22 @@ namespace GlucoMan.Maui;
 
 public partial class BackPicturePage : ContentPage
 {
-	public BackPicturePage()
-	{
-		InitializeComponent();
-	}
+    CirclesDrawable cerchioD;
+    public BackPicturePage()
+    {
+        InitializeComponent();
+
+        // Istanziazione dell'oggetto che disegnerà il (i) cerchio
+        cerchioD = new CirclesDrawable();
+        // collegamento del Drawable al GraphicsView
+        cerchiGraphicsView.Drawable = cerchioD;
+        cerchiGraphicsView.Background = Color.FromRgba(255, 255, 0, 100);
+        var children = cerchiGraphicsView.GetChildElements(new Point(100, 100));
+        //cerchiGraphicsView.Width = Immagine.Width;
+
+        // "reset" del GraphicsView, che rende necessario il lancio del metodo Draw (prima prova di disegno cerchio) 
+        cerchiGraphicsView.Invalidate();
+    }
     private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
         // Position relative to the container view (the image).
@@ -15,6 +27,13 @@ public partial class BackPicturePage : ContentPage
         {
             double x = relativeToContainerPosition.Value.X;
             double y = relativeToContainerPosition.Value.Y;
+
+            cerchioD.LeftCerchio = (float)x;
+            cerchioD.TopCerchio = (float)y;
+            // rende non valido tutto quello che c'è nel cerchiGraphicsView.
+            // Pertanto il sistema deve lanciare il metodo Draw() per ridisegnare 
+            // TUTTA la grafica che c'era
+            this.cerchiGraphicsView.Invalidate();
         }
     }
 }
---
+++ GlucoMan.Maui/CirclesDrawable.cs
@@ -0,0 +1,22 @@
+﻿namespace GlucoMan.Maui
+{
+    internal class CirclesDrawable : IDrawable
+    {
+        internal float LeftCerchio { get; set; } = 0;
+        internal float TopCerchio { get; set; } = 0;
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            // colore con cui disegnerò la linea del cerchio
+            canvas.StrokeColor = Colors.Red;
+            // dimensione della linea che disegnerò
+            canvas.StrokeSize = 4;
+            // colore di riempimento delle forme che disegnerò
+            canvas.FillColor = Colors.Blue; // NON MI FUNZIONA, MA DOVREBBE!!
+
+            // disegno del cerchio
+            canvas.DrawEllipse(LeftCerchio, TopCerchio, 20, 20);
+            //canvas.DrawEllipse(LeftCerchio, TopCerchio, 4, 4);
+        }
+    }
+}
